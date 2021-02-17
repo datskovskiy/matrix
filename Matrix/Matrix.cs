80ba59937e559b6d9893abae1e967b62bb43ebca
anton.datskovskiy <datskovskiy.anton@gmail.com>
2021-02-17 09:29:09 +0200

    Implemented code

--- Matrix/Matrix.cs
+++ Matrix/Matrix.cs
@@ -1,34 +1,44 @@
 using System;
+using System.Runtime.Serialization;
 
 namespace MatrixLibrary
 {
-    // TODO: Create custom exception "MatrixException"
-    
+    [Serializable]
+    public class MatrixException : Exception
+    {
+        public MatrixException()
+        {
+        }
+
+        public MatrixException(string message) : base(message)
+        {
+        }
+
+        public MatrixException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        protected MatrixException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+
     public class Matrix : ICloneable
     {
         /// <summary>
         /// Number of rows.
         /// </summary>
-        public int Rows
-        {
-            get => throw new NotImplementedException();
-        }
+        public int Rows { get; }
 
         /// <summary>
         /// Number of columns.
         /// </summary>
-        public int Columns
-        {
-            get => throw new NotImplementedException();
-        }
+        public int Columns { get; }
         
         /// <summary>
         /// Gets an array of floating-point values that represents the elements of this Matrix.
         /// </summary>
-        public double[,] Array
-        {
-            get => throw new NotImplementedException();
-        }
+        public double[,] Array { get; }
         
         /// <summary>
         /// Initializes a new instance of the <see cref="Matrix"/> class.
@@ -38,7 +48,23 @@ namespace MatrixLibrary
         /// <exception cref="ArgumentOutOfRangeException"></exception>
         public Matrix(int rows, int columns)
         {
-            throw new NotImplementedException();
+            if (rows < 0) 
+                throw new ArgumentOutOfRangeException(nameof(rows), "rows cant be less then zero");
+
+            if (columns < 0) 
+                throw new ArgumentOutOfRangeException(nameof(columns), "columns cant be less then zero");
+
+            Rows = rows;
+            Columns = columns;
+            Array = new double[Rows, Columns];
+
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Columns; j++)
+                {
+                    Array[i, j] = 0;
+                }
+            }
         }
 
         /// <summary>
@@ -48,7 +74,12 @@ namespace MatrixLibrary
         /// <exception cref="ArgumentNullException"></exception>
         public Matrix(double[,] array)
         {
-            throw new NotImplementedException();
+            if (array == null)
+                throw new ArgumentNullException(nameof(array), "array cant be null");
+
+            Rows = array.GetLength(0);
+            Columns = array.GetLength(1);
+            Array = array;
         }
 
         /// <summary>
@@ -59,8 +90,26 @@ namespace MatrixLibrary
         /// <exception cref="ArgumentException"></exception>
         public double this[int row, int column]
         {
-            get => throw new NotImplementedException();
-            set => throw new NotImplementedException();
+            get
+            {
+                if (row < 0 || row > Array.GetLength(0))
+                    throw new ArgumentException(nameof(row));
+
+                if (column < 0 || column > Array.GetLength(1))
+                    throw new ArgumentException(nameof(column));
+
+                return Array[row, column];
+            }
+            set
+            {
+                if (row < 0 || row > Array.GetLength(0))
+                    throw new ArgumentException(nameof(row));
+
+                if (column < 0 || column > Array.GetLength(1))
+                    throw new ArgumentException(nameof(column));
+
+                Array[row, column] = value;
+            }
         }
 
         /// <summary>
@@ -82,7 +131,28 @@ namespace MatrixLibrary
         /// <exception cref="MatrixException"></exception>
         public static Matrix operator +(Matrix matrix1, Matrix matrix2)
         {
-            throw new NotImplementedException();
+            if (matrix1 == null)
+                throw new ArgumentNullException(nameof(matrix1));
+
+            if(matrix2 == null)
+                throw new ArgumentNullException(nameof(matrix2));
+
+            if (matrix1.Rows != matrix2.Rows || matrix1.Columns != matrix2.Columns)
+                throw new MatrixException($"Cannot compute sum, matrix dimensions do not match:\n\t" +
+                                          $"matrix1 : {matrix1.Rows}x{matrix1.Columns}\n\t" +
+                                          $"matrix2 : {matrix2.Rows}x{matrix2.Columns}");
+
+            var result = new Matrix(matrix1.Rows, matrix1.Columns);
+
+            for (int i = 0; i < matrix1.Rows; i++)
+            {
+                for (int j = 0; j < matrix2.Columns; j++)
+                {
+                    result[i, j] = matrix1[i, j] + matrix2[i, j];
+                }
+            }
+
+            return result;
         }
 
         /// <summary>
@@ -95,7 +165,28 @@ namespace MatrixLibrary
         /// <exception cref="MatrixException"></exception>
         public static Matrix operator -(Matrix matrix1, Matrix matrix2)
         {
-            throw new NotImplementedException();
+            if (matrix1 == null)
+                throw new ArgumentNullException(nameof(matrix1));
+
+            if (matrix2 == null)
+                throw new ArgumentNullException(nameof(matrix2));
+
+            if (matrix1.Rows != matrix2.Rows || matrix1.Columns != matrix2.Columns)
+                throw new MatrixException($"Cannot compute subtract, matrix dimensions do not match:\n\t" +
+                                          $"matrix1 : {matrix1.Rows}x{matrix1.Columns}\n\t" +
+                                          $"matrix2 : {matrix2.Rows}x{matrix2.Columns}");
+
+            var result = new Matrix(matrix1.Rows, matrix1.Columns);
+
+            for (int i = 0; i < matrix1.Rows; i++)
+            {
+                for (int j = 0; j < matrix2.Columns; j++)
+                {
+                    result[i, j] = matrix1[i, j] - matrix2[i, j];
+                }
+            }
+
+            return result;
         }
 
         /// <summary>
@@ -108,7 +199,31 @@ namespace MatrixLibrary
         /// <exception cref="MatrixException"></exception>
         public static Matrix operator *(Matrix matrix1, Matrix matrix2)
         {
-            throw new NotImplementedException();
+            if (matrix1 == null)
+                throw new ArgumentNullException(nameof(matrix1));
+
+            if (matrix2 == null)
+                throw new ArgumentNullException(nameof(matrix2));
+
+            if (matrix1.Columns != matrix2.Rows)
+                throw new MatrixException($"Cannot compute product, matrix dimensions do not match:\n\t" +
+                                          $"matrix1 : {matrix1.Rows}x{matrix1.Columns}\n\t" +
+                                          $"matrix2 : {matrix2.Rows}x{matrix2.Columns}");
+
+            var result = new Matrix(matrix1.Rows, matrix2.Columns);
+
+            for (int i = 0; i < result.Rows; i++)
+            {
+                for (int j = 0; j < result.Columns; j++)
+                {
+                    for (int k = 0; k < matrix1.Columns; k++)
+                    {
+                        result[i, j] += matrix1[i, k] * matrix2[k, j];
+                    }
+                }
+            }
+
+            return result;
         }
 
         /// <summary>
@@ -119,7 +234,7 @@ namespace MatrixLibrary
         /// <exception cref="MatrixException"></exception>
         public Matrix Add(Matrix matrix)
         {
-            throw new NotImplementedException();
+            return this + matrix;
         }
 
         /// <summary>
@@ -130,7 +245,7 @@ namespace MatrixLibrary
         /// <exception cref="MatrixException"></exception>
         public Matrix Subtract(Matrix matrix)
         {
-            throw new NotImplementedException();
+            return this - matrix;
         }
 
         /// <summary>
@@ -141,7 +256,7 @@ namespace MatrixLibrary
         /// <exception cref="MatrixException"></exception>
         public Matrix Multiply(Matrix matrix)
         {
-            throw new NotImplementedException();
+            return this * matrix;
         }
 
         /// <summary>
